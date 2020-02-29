e93b2c64afd77fb5bcad3fe2ea66e168aa83c50c
Jason Coon <jasoncoon@users.noreply.github.com>
2020-02-29 08:19:19 -0600

    Fibonacci256 branch

--- esp8266-fastled-webserver.ino
+++ esp8266-fastled-webserver.ino
@@ -55,7 +55,7 @@ ESP8266HTTPUpdateServer httpUpdateServer;
 #define DATA_PIN      D1
 #define LED_TYPE      WS2812B
 #define COLOR_ORDER   GRB
-#define NUM_LEDS      64
+#define NUM_LEDS      256
 
 #define MILLI_AMPS         2000 // IMPORTANT: set the max milli-Amps of your power supply (4A = 4000mA)
 #define FRAMES_PER_SECOND  120  // here you can control the speed. With the Access Point / Web Server the animations run a bit slower.
